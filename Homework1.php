87d16982e7136314ad26f1fe6735625768692d2a
mandy10435 <mandy10435@gmail.com>
2017-05-20 02:32:26 +0800

    PHP TUTORIAL -2

---
+++ Homework1.php
@@ -0,0 +1,55 @@
+<a type="button" href="Homework1.php">HW 01</a>
+<a type="button" href="Homework2.php">HW 02</a>
+<a type="button" href="Homework3.php">HW 03</a>
+<h1>PHP 加減乘除 Assignment 1/3</h1>
+<p>EX: 10/3=3.33333 ===> 顯示3餘1</p>
+<?php
+if (isset($_GET["x"])){
+    $x = $_GET["x"];
+    $y = $_GET["y"];
+    $s = $_GET["taskOption"];
+    $e = "=";
+}
+?>
+<form>
+    <input type="text" id="x" name="x" value="<?php echo $x; ?>">
+    <!-- Initializing Name With An Array-->
+    <select name="taskOption">
+        <option value="<?php echo $s?>">--select one--</option>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <input type="text" id="y" name="y" value="<?php echo $y; ?>">
+    <input type="submit" value="==>">
+    <sapn id="result">
+        <?php
+            echo "$x $s $y $e $r";
+            if ($s == "+") {
+                $r = $x + $y;
+                echo $r;
+            } elseif ($s == "-") {
+                $r = $x - $y;
+                echo $r;
+            } elseif ($s == "*") {
+                $r = $x * $y;
+                echo $r;
+            } elseif ($s == "/") {
+                if ($x % $y == 0) {
+                    $r = $x / $y;
+                    echo $r;
+                } else {
+                    $a = floor($x / $y);
+                    $l = $x % $y;
+                    $r = "$a 餘 $l";
+                    echo $r;
+                }
+            } else {
+                $r = "Please Type Numbers!";
+                echo $r;
+            }
+            ?>
+
+    </sapn>
+</form>
