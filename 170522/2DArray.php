829ec32874ae1279cab677f1ad3e1d1bda77f83f
mandy10435 <mandy10435@gmail.com>
2017-05-22 17:31:26 +0800

    PHP TUTORIAL -2

--- 170522/2DArray.php
+++ 170522/2DArray.php
@@ -3,7 +3,7 @@
     $b[] = $a;// $b[0] ==> array(1,2,3);
     $b[] = array(2,4,5,7);
     $b[] = array(2,7,5,7);
-    echo count($b[0]).'<hr>';//代表第三行
+    echo count($b[0]).'<hr>';////代表第三行
     echo count($b[1]).'<hr>';//代表第四行的
     echo count($b).'<hr>';//第三行到第五行
     //永遠只看ㄧ維 因為php是線型運算的
