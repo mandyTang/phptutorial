ebc11d39d5eda0e3f7a396d1cb67812682e0347b
mandy10435 <mandy10435@gmail.com>
2017-05-23 19:44:06 +0800

    PHP TUTORIAL -2

---
+++ 170523/Poker/pokerImg.php
@@ -0,0 +1,29 @@
+<h1>Poker</h1>
+<?php
+$poker=range(1,52);
+$names=array('Nick','Mandy','John','Anna');
+shuffle($poker);//洗牌
+
+foreach ($names as $name){
+    echo $name.'<br>';
+}
+//分發四個人 $i ==> $key
+foreach ($poker as $i => $card){
+    $player[$i%4][(int)($i/4)]=$card;
+    //echo $card.'<br>';
+}
+echo '<hr>';
+?>
+<table width="100%" border="0">
+        <?php
+            foreach($player as $sb){
+                sort($sb);
+                echo '<tr>';
+                foreach ($sb as $card) {
+                    echo '<td>'."<img src='img/Artboard%20{$card}.svg'>".'</td>';
+                }
+                echo '</tr>';
+            }
+
+        ?>
+</table>
