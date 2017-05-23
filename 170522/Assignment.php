aa6e5a82dd1bc523e11cc007ef5f12765078e44e
mandy10435 <mandy10435@gmail.com>
2017-05-23 09:08:44 +0800

    PHP TUTORIAL -2

--- 170522/Assignment.php
+++ 170522/Assignment.php
@@ -1,25 +1,44 @@
 <h1>Poker</h1>
 <ul>
     <li>骰子</li>
-    <li></li>
 </ul>
 <hr>
 <?php
-    $dice = array();
-    for ($i=0; $i<6; $i++){
-    //檢查機制
-        do{
-            $temp = rand(0,5);
-            $repeat = false;
-            for($j=0; $j<$i; $j++){
-                if($dice[$j]==$temp){
-                    $repeat = true;
-                    break;
-                }
-            }
-        } while ($repeat);
-    $dice[] = $temp;
+    $dice = array(1,2,3,4,5,6);
+    for ($i=0;$i<6;$i++){
+        shuffle($dice);
+        echo 'Choose ' . $dice[0].'<br>';
+        echo $dice[1].$dice[2].$dice[3].$dice[4].$dice[5].'<br>';
+        echo '<hr>';
+        break;
+    }
+    shuffle($dice);
+    echo 'Choose ' . $dice[0].'<br>';
+    echo $dice[1].$dice[2].$dice[3].$dice[4].$dice[5].'<br>';
+    echo '<hr>';
 
-    echo $dice[$i].'<br>';
-}
+    $dice2 = array($dice[1],$dice[2],$dice[3],$dice[4],$dice[5]);
+    shuffle($dice2);
+    echo 'Choose ' . $dice2[0].'<br>';
+    echo $dice2[1].$dice2[2].$dice2[3].$dice2[4].'<br>';
+    echo '<hr>';
+
+    $dice3 = array($dice2[1],$dice2[2],$dice2[3],$dice[4]);
+    shuffle($dice3);
+    echo 'Choose ' . $dice3[0].'<br>';
+    echo $dice3[1].$dice3[2].$dice3[3].'<br>';
+    echo '<hr>';
+
+    $dice4 = array($dice3[1],$dice3[2],$dice3[3]);
+    shuffle($dice4);
+    echo 'Choose ' . $dice4[0].'<br>';
+    echo $dice4[1].$dice4[2].'<br>';
+    echo '<hr>';
+
+    $dice5 = array($dice4[1],$dice4[2]);
+    shuffle($dice5);
+    echo 'Choose ' . $dice5[0].'<br>';
+    echo $dice5[1].'<br>';
+    echo '<hr>';
+
 ?>
